4ce875232021b0e3945cf39d9c492efeddf4a06f
Serve Rabani <servarabani@gmail.com>
2026-05-17 13:58:20 +0330

    Phase 2 - Multi client support added

--- server/server.py
+++ server/server.py
@@ -1,26 +1,41 @@
 import socket
+import threading
 
 HOST = '127.0.0.1'
 PORT = 5000
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server.bind((HOST, PORT))
-
 server.listen()
 
-print("Server is listening...")
+print("Server is running...")
 
-client_socket, client_address = server.accept()
+def handle_client(client_socket, client_address):
+    print(f"[NEW CONNECTION] {client_address}")
 
-print(f"Connected to {client_address}")
+    while True:
+        try:
+            message = client_socket.recv(1024).decode()
 
-message = client_socket.recv(1024).decode()
+            if not message:
+                break
 
-print(message)
+            print(f"{client_address}: {message}")
 
-client_socket.send("hello client".encode())
+            client_socket.send("Message received".encode())
 
-client_socket.close()
+        except:
+            break
 
-server.close()
+    print(f"[DISCONNECTED] {client_address}")
+    client_socket.close()
+
+
+while True:
+    client_socket, client_address = server.accept()
+
+    thread = threading.Thread(
+        target=handle_client,
+        args=(client_socket, client_address)
+    )
+    thread.start()
