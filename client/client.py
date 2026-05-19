a10ce210b1eb8bbecbb35b8b56551cb7df3a49e0
Serve Rabani <servarabani@gmail.com>
2026-05-19 09:56:49 +0330

    Phase 3 - Broadcast chat implemented

--- client/client.py
+++ client/client.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 HOST = '127.0.0.1'
 PORT = 5000
@@ -7,10 +8,47 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((HOST, PORT))
 
-client.send("hello server".encode())
+print("Connected to chat server")
 
-response = client.recv(1024).decode()
 
-print(response)
+def receive_messages():
 
-client.close()
+    while True:
+
+        try:
+
+            message = client.recv(1024).decode()
+
+            print(message)
+
+        except:
+
+            print("Disconnected from server")
+
+            client.close()
+
+            break
+
+
+def send_messages():
+
+    while True:
+
+        message = input()
+
+        client.send(message.encode())
+
+        if message == "/exit":
+
+            client.close()
+
+            break
+
+
+receive_thread = threading.Thread(target=receive_messages)
+
+send_thread = threading.Thread(target=send_messages)
+
+receive_thread.start()
+
+send_thread.start()
--- server/server.py
+++ server/server.py
@@ -5,37 +5,74 @@ HOST = '127.0.0.1'
 PORT = 5000
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 server.bind((HOST, PORT))
+
 server.listen()
 
 print("Server is running...")
 
+clients = []
+
+
+def broadcast(message, sender_socket):
+
+    for client in clients:
+
+        if client != sender_socket:
+
+            try:
+                client.send(message.encode())
+
+            except:
+                client.close()
+
+                if client in clients:
+                    clients.remove(client)
+
+
 def handle_client(client_socket, client_address):
+
     print(f"[NEW CONNECTION] {client_address}")
 
+    clients.append(client_socket)
+
     while True:
+
         try:
+
             message = client_socket.recv(1024).decode()
 
             if not message:
                 break
 
-            print(f"{client_address}: {message}")
+            if message == "/exit":
+                break
 
-            client_socket.send("Message received".encode())
+            full_message = f"{client_address[0]}: {message}"
+
+            print(full_message)
+
+            broadcast(full_message, client_socket)
 
         except:
             break
 
     print(f"[DISCONNECTED] {client_address}")
+
+    if client_socket in clients:
+        clients.remove(client_socket)
+
     client_socket.close()
 
 
 while True:
+
     client_socket, client_address = server.accept()
 
     thread = threading.Thread(
         target=handle_client,
         args=(client_socket, client_address)
     )
+
     thread.start()
